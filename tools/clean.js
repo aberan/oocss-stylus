732b53301e2fa93920449b8a5fdd22a464368560
Anthony Beran <aberan@nxnw.net>
2013-08-02 11:53:59 -0700

    fixed path for fs.extra require

--- tools/clean.js
+++ tools/clean.js
@@ -1,6 +1,6 @@
 var config = require('./config.js');
 //start fs module
-var fs = require('../node_modules/fs.extra');
+var fs = require('fs.extra');
 
 var src = config.src;
 var build = config.build;
