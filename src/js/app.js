9d74857caf3b1061b5163b7c2990b8f6856835b3
Anthony Beran <aberan@nxnw.net>
2013-09-19 12:25:41 -0700

    updated app.js to correctly detect < IE9. Removed no longer used fout.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ requirejs.config({
 	paths : {
 		jquery: ['//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min','vendor/jquery-2.0.3.min'],
 		jquery_old: ['//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min','vendor/jquery-1.10.2.min'],
-		app: './vendor'
+		app: 'vendor'
 	},
 	shim: {
 		plugins: {
@@ -15,7 +15,7 @@ requirejs.config({
 	}
 });
 
-if( !'__proto__' in {} ) {
+if( !document.addEventListener ) {
 	requirejs.config({
 		map: {
 			'*': {
