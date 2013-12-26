695da45fe3c49d4fc46ef712374bea0ecc0bc2aa
Anthony Beran <aberan@nxnw.net>
2013-12-26 08:47:57 -0800

    added requireJS watch config to do no optimization for faster compile times

---
+++ app.build-watch.js
@@ -0,0 +1,14 @@
+({
+	appDir: "src/js",
+	baseUrl: ".",
+	dir: "build/js",
+	keepBuildDir: true,
+	preserveLicenseComments: false,
+	optimize: "none",
+	paths: {
+		jquery: "empty:",
+		jquery_old: "empty:",
+		app: './vendor'
+	},
+	name: "main"
+})
